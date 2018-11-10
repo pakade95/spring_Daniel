26889eb46d3f54ca90fb8282d99f01f3ed868645
Dominik Kowalik <Dominik.Kowalik@impaqgroup.com>
2018-11-10 21:21:33 +0100

    fix dependencies and place index in webapp file

--- spring-boot-mvc/src/main/java/pl/danieltrzaskoma/springbootmvc/Controller/HomeController.java
+++ spring-boot-mvc/src/main/java/pl/danieltrzaskoma/springbootmvc/Controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(path = "/")
     public String home(){
-        return"index";
+        return "index";
     }
 }
